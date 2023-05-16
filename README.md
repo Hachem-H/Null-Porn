8dacf2a29f1aa85efbf9be1ccef04cbd43c09c2a
Hachem-H <hh.naram@gmail.com>
2023-05-16 14:29:01 +0300

    Project Setup

---
+++ README.md
@@ -0,0 +1,9 @@
+![logo](res/Logo.png)
+
+# NullPorn
+NullPorn is a project born out of frustration with the increasing presence of pornographic and explicit content on platforms such as [YouTube](https://youtube.com)/[YouTube Shorts](https://shorts.youtube.com) and [TikTok](https://tiktok.com). These platforms have been inundated with spam bots that not only disrupt the user experience but also display offensive and inappropriate material. In response to this issue, I have taken the initiative to create a set of scripts that aim to mitigate the problem, including a DDoS flood script and various DNS tracker scripts.
+
+## Disclaimer
+Although NullPorn is an open-source project and its repository is publicly available, it is crucial to use this software responsibly and ethically. The purpose of this project is solely educational, intended to shed light on the techniques involved and the potential vulnerabilities of certain systems. **Under no circumstances should this project be used for any malicious or harmful activities.** Any misuse or unethical use of the scripts is strictly prohibited.
+
+Please respect the principles of responsible usage and ensure that you adhere to legal and ethical standards when engaging with this project.
---
+++ premake5.lua
@@ -0,0 +1,58 @@
+workspace "NullPorn"
+    configurations { "Debug", "Release", "Distribution" }
+    architecture "x86_64"
+    startproject "NullPorn-Gui"
+    systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "NP_DEBUG"
+        optimize "Off"
+        symbols "On"
+    filter "configurations:Release"
+        defines "NP_RELEASE"
+        optimize "On"
+        symbols "On"
+    filter "configurations:Distribution"
+        defines "NP_DIST"
+        optimize "On"
+        symbols "Off"
+
+project "NullPorn-Core"
+    location "%{wks.location}/%{prj.name}"
+    kind "StaticLib"
+    language "C"
+
+    targetdir  "%{wks.location}/bin/"
+    objdir "%{wks.location}/bin-int/%{prj.name}-%{cfg.buildcfg}/"
+
+    files { "%{prj.location}/src/**.c",
+            "%{prj.location}/src/**.h" }
+    includedirs { "%{prj.location}/src" }
+
+project "NullPorn-GUI"
+	location "%{wks.location}/%{prj.name}"
+	kind "ConsoleApp"
+	language "C"
+
+	targetdir  "%{wks.location}/bin/"
+	objdir "%{wks.location}/bin-int/%{prj.name}-%{cfg.buildcfg}/"
+
+	files { "%{prj.location}/src/**.c",
+			"%{prj.location}/src/**.h" }
+	includedirs { "%{prj.location}/src",
+				  "%{wks.location}/NullPorn-Core/src" }
+	links { "NullPorn-Core" }
+
+project "NullPorn-CLI"
+    location "%{wks.location}/%{prj.name}"
+    kind "ConsoleApp"
+    language "C"
+
+    targetdir  "%{wks.location}/bin/"
+    objdir "%{wks.location}/bin-int/%{prj.name}-%{cfg.buildcfg}/"
+
+    files { "%{prj.location}/src/**.c",
+            "%{prj.location}/src/**.h" }
+    includedirs { "%{prj.location}/src",
+                  "%{wks.location}/NullPorn-Core/src" }
+    links { "NullPorn-Core" }
