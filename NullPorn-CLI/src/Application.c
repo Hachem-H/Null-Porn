10ea14a43c576bc1a17e3014842d9fb7b05764e3
Hachem-H <hh.naram@gmail.com>
2023-05-18 22:16:35 +0300

    Scrolling Window

--- NullPorn-CLI/src/Application.c
+++ NullPorn-CLI/src/Application.c
@@ -1,34 +1,150 @@
 #include <ncurses.h>
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 int main()
 {
     char* windowLabel = "NULL PORN";
     char* addURLLabel = "[Add URL]";
-    char* addIpLabel  = "[Add IP]";
+    char* addIpLabel = "[Add IP]";
     char* attackLabel = "[ATTACK]";
 
     initscr();
+    cbreak();
+    keypad(stdscr, true);
+    noecho();
 
-    WINDOW* topMenu = newwin(3, COLS, 0, 0);
-    WINDOW* content = newwin(LINES, COLS, 3, 0);
+    WINDOW* menu = newwin(3, COLS, 0, 0);
+    WINDOW* content = newwin(LINES - 3, COLS, 3, 0);
     refresh();
 
-    box(topMenu, 0, 0);
-    box(content, 0, 0);
+    scrollok(menu, false);
+    scrollok(content, true);
 
-    mvwprintw(topMenu,    0, (COLS-sizeof(windowLabel))/2, windowLabel);
+    mvwprintw(menu, 0, (COLS - strlen(windowLabel)) / 2, windowLabel);
     mvwprintw(content, 0, 1, "Attacked URLs/IPs");
 
-    for (int i = 0; i < 50; i++)
-        mvwprintw(content, i+1, 2, "[-] 127.0.0.%d", i+1);
+    int numIds = 150;
+    char* temporaryIds[numIds];
+    for (int i = 0; i < numIds; i++)
+    {
+        temporaryIds[i] = malloc(12 * sizeof(char));
+        sprintf(temporaryIds[i], "127.0.0.%d", i + 1);
+    }
 
-    mvwprintw(topMenu, 1, 2,                          addURLLabel);
-    mvwprintw(topMenu, 1, sizeof(addURLLabel)+4,      addIpLabel);
-    mvwprintw(topMenu, 1, COLS-sizeof(attackLabel)-2, attackLabel);
+    int contentOffset = 0;
+    int visibleLines = LINES - 4;
+    int numVisibleIds = (numIds < visibleLines) ? numIds : visibleLines;
 
-    wrefresh(topMenu);
+    for (int i = 0; i < numVisibleIds; i++)
+        mvwprintw(content, i + 1, 2, "[-] %s", temporaryIds[i]);
+
+    mvwprintw(menu, 1, 2, addURLLabel);
+    mvwprintw(menu, 1, strlen(addURLLabel) + 4, addIpLabel);
+    mvwprintw(menu, 1, COLS - strlen(attackLabel) - 2, attackLabel);
+
+    int cursorState = 0;
+    int cursorX = 1;
+    int cursorY = 1;
+    int key;
+
+    box(menu, 0, 0);
+    box(content, 0, 0);
+
+    wmove(menu, cursorY, cursorX);
     wrefresh(content);
-    
-    getch();
+    wrefresh(menu);
+
+    while ((key = getch()) != KEY_F(1))
+    {
+
+        switch (key)
+        {
+            case KEY_LEFT:
+                if (cursorX > 1)
+                    cursorX--;
+                break;
+            case KEY_RIGHT:
+                if (cursorX < COLS - 2)
+                    cursorX++;
+                break;
+
+            case KEY_UP:
+            {
+                if (cursorY > 1)
+                    cursorY--;
+                else if (cursorState % 2 != 0)
+                {
+                    if (contentOffset > 0)
+                    {
+                        contentOffset--;
+                        werase(content);
+                        box(content, 0, 0);
+                        for (int i = 0; i < numVisibleIds; i++)
+                            mvwprintw(content, i + 1, 2, "[-] %s", temporaryIds[i + contentOffset]);
+                        wrefresh(content);
+                    }
+                }
+            }
+            break;
+
+            case KEY_DOWN:
+            {
+                if (cursorState % 2 == 0)
+                {
+                    if (cursorY < 1)
+                        cursorY++;
+                }
+                else
+                {
+                    if (cursorY < numVisibleIds-1)
+                        cursorY++;
+                    else
+                    {
+                        if (contentOffset + visibleLines - 1 < numIds)
+                        {
+                            contentOffset++;
+                            werase(content);
+                            box(content, 0, 0);
+                            for (int i = 0; i < numVisibleIds-1; i++)
+                                mvwprintw(content, i + 1, 2, "[-] %s", temporaryIds[i + contentOffset]);
+                            wrefresh(content);
+                        }
+                    }
+                }
+            }
+            break;
+
+            case '\t':
+            {
+                cursorState++;
+                cursorX = 1;
+                cursorY = 1;
+            }
+            break;
+
+            default:
+                break;
+        }
+
+        box(menu, 0, 0);
+        box(content, 0, 0);
+
+        if (cursorState % 2 == 0)
+        {
+            wmove(menu, cursorY, cursorX);
+            wrefresh(menu);
+        }
+        else
+        {
+            wmove(content, cursorY, cursorX);
+            wrefresh(content);
+        }
+    }
+
+    for (int i = 0; i < numIds; i++)
+        free(temporaryIds[i]); 
     endwin();
 }
