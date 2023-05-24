c169284cf0731b1757d270959db9d3414656c14d
Hachem-H <hh.naram@gmail.com>
2023-05-24 19:23:13 +0300

    Reset GUI

--- premake5.lua
+++ premake5.lua
@@ -17,57 +17,6 @@ workspace "NullPorn"
         symbols "Off"
 
 group "Dependencies"
-    project "glfw"
-        location "%{wks.location}/Vendor/%{prj.name}"
-        staticruntime "On"
-        kind "StaticLib"
-        language "C"
-    
-        targetdir  "%{wks.location}/bin/%{prj.name}"
-        objdir "%{wks.location}/bin-int/%{prj.name}-%{cfg.buildcfg}/"
-    
-        files { "%{prj.location}/src/glfw_config.h",
-    	        "%{prj.location}/src/context.c",
-    	        "%{prj.location}/src/init.c",
-    	        "%{prj.location}/src/input.c",
-    	        "%{prj.location}/src/monitor.c",
-    	        "%{prj.location}/src/null_init.c",
-    	        "%{prj.location}/src/null_joystick.c",
-    	        "%{prj.location}/src/null_monitor.c",
-    	        "%{prj.location}/src/null_window.c",
-    	        "%{prj.location}/src/platform.c",
-    	        "%{prj.location}/src/vulkan.c",
-    	        "%{prj.location}/src/window.c", }
-        includedirs "%{prj.location}/include/"
-        
-        filter "system:linux"
-            pic "On"
-            defines "_GLFW_X11"
-    
-            files { "%{prj.location}/src/x11_init.c",
-    		        "%{prj.location}/src/x11_monitor.c",
-    		        "%{prj.location}/src/x11_window.c",
-    		        "%{prj.location}/src/xkb_unicode.c",
-    		        "%{prj.location}/src/posix_time.c",
-    		        "%{prj.location}/src/posix_poll.c",
-    		        "%{prj.location}/src/posix_thread.c",
-    		        "%{prj.location}/src/posix_module.c",
-    		        "%{prj.location}/src/glx_context.c",
-    		        "%{prj.location}/src/egl_context.c",
-    		        "%{prj.location}/src/osmesa_context.c",
-    		        "%{prj.location}/src/linux_joystick.c" }
-
-    project "glew"
-        location "%{wks.location}/Vendor/%{prj.name}"
-        staticruntime "On"
-        kind "StaticLib"
-        language "C"
-
-        targetdir  "%{wks.location}/bin/%{prj.name}"
-        objdir "%{wks.location}/bin-int/%{prj.name}-%{cfg.buildcfg}/"
-
-        files { "%{prj.location}/src/**.c" }
-        includedirs "%{prj.location}/include/"
 group ""
         
 project "NullPorn-Core"
@@ -129,4 +78,4 @@ project "NullPorn-GUI"
                   "%{wks.location}/Vendor/glfw/include",
                   "%{wks.location}/Vendor/Nuklear",  
                   "%{wks.location}/Vendor/stb",  }
-	links { "NullPorn-Core", "glfw", "glew", "GL", "m" }
+	links { "NullPorn-Core", }
