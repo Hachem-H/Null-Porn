66bc553c0ed52986e44e9f328ba561b7e251fcd3
Hachem-H <hh.naram@gmail.com>
2023-05-26 12:33:19 +0300

    Finish UI

--- NullPorn-GUI/src/Application.c
+++ NullPorn-GUI/src/Application.c
@@ -4,12 +4,47 @@
 #include <raygui.h>
 
 #include <stb_ds.h>
+#include <DDoS.h>
+
+#include <pthread.h>
+#include <regex.h>
 
 #define WINDOW_WIDTH  1720
 #define WINDOW_HEIGHT 967
 
 static char** GlobalIPs  = NULL;
 static char** GlobalURLs = NULL;
+
+void* FloodThread(void* arg)
+{
+    char* ipAddress = (char*)arg;
+    Flood(ipAddress);
+    return NULL;
+}
+
+void* DNSLookupThread(void* arg)
+{
+    char* url = (char*)arg;
+    char* ipAddress = DNSLookup(url);
+    if (ipAddress != NULL) 
+        Flood(ipAddress);
+    return NULL;
+}
+
+static bool ValidURL(const char* url)
+{
+    regex_t regex;
+    int ret;
+    const char* pattern = "^(https?|ftp)://[^\\s/$.?#].[^\\s]*$";
+
+    ret = regcomp(&regex, pattern, REG_EXTENDED);
+    if (ret != 0) 
+        return false;
+
+    ret = regexec(&regex, url, 0, NULL, 0);
+    regfree(&regex);
+    return !ret;
+}
 
 int main()
 {
@@ -20,11 +55,13 @@ int main()
     Rectangle panelContentRect = { 0, 0, panelRect.width-15, 0 };
     Vector2   panelScroll      = { 99, 0 };
 
+    bool showError = false;
     bool showAddIP  = false;
     bool showAddURL = false;
 
     char urlBuffer[0x800] = {0};
     char ipBuffer[0x800] = {0};
+    char* errorType;
 
     while (!WindowShouldClose())
     {
@@ -34,7 +71,7 @@ int main()
         int numEntries = arrlen(GlobalIPs)+arrlen(GlobalURLs);
         panelContentRect.height = numEntries*1.2*30+100;
 
-        if (!showAddIP && !showAddURL)
+        if (!showAddIP && !showAddURL && !showError)
         {
             Rectangle content = GuiScrollPanel(panelRect, NULL, panelContentRect, &panelScroll);
             BeginScissorMode(content.x, content.y, content.width, content.height);
@@ -84,7 +121,6 @@ int main()
             GuiTextBox((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2+100, WINDOW_WIDTH/2, 50 }, ipBuffer, 0x800, true);
 
             GuiSetStyle(DEFAULT, TEXT_SIZE, 28);
-            GuiSetStyle(BUTTON, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
             if (GuiButton((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2+200, WINDOW_WIDTH/4-50, 40 }, "Cancel"))
             {
                 memset(ipBuffer, 0, 0x800);
@@ -93,7 +129,14 @@ int main()
 
             if (GuiButton((Rectangle) { WINDOW_WIDTH/2+50, WINDOW_HEIGHT/2+200, WINDOW_WIDTH/4-50, 40 }, "Okay"))
             {
-                arrput(GlobalIPs, strdup(ipBuffer));
+                if (ValidIP(ipBuffer))
+                    memset(ipBuffer, 0, 0x800);
+                else 
+                {
+                    errorType = "Invalid IP";
+                    showError = true;
+                }
+
                 memset(ipBuffer, 0, 0x800);
                 showAddIP = false;
             }
@@ -108,7 +151,6 @@ int main()
             GuiTextBox((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2+100, WINDOW_WIDTH/2, 50 }, urlBuffer, 0x800, true);
 
             GuiSetStyle(DEFAULT, TEXT_SIZE, 28);
-            GuiSetStyle(BUTTON, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
             if (GuiButton((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2+200, WINDOW_WIDTH/4-50, 40 }, "Cancel"))
             {
                 memset(urlBuffer, 0, 0x800);
@@ -117,10 +159,30 @@ int main()
 
             if (GuiButton((Rectangle) { WINDOW_WIDTH/2+50, WINDOW_HEIGHT/2+200, WINDOW_WIDTH/4-50, 40 }, "Okay"))
             {
-                arrput(GlobalURLs, strdup(urlBuffer));
+                if (ValidURL(urlBuffer))
+                    arrput(GlobalURLs, strdup(urlBuffer));
+                else
+                {
+                    errorType = "Invalid URL";
+                    showError = true;
+                }
+
                 memset(urlBuffer, 0, 0x800);
                 showAddURL = false;
             }
+        }
+
+        if (showError)
+        {
+            GuiSetStyle(DEFAULT, TEXT_SIZE, 80);
+            GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
+            GuiLabel((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2-100, WINDOW_WIDTH/2, 80 }, "ERROR");
+            GuiSetStyle(DEFAULT, TEXT_SIZE, 48);
+            GuiLabel((Rectangle) { WINDOW_WIDTH/4, WINDOW_HEIGHT/2+100, WINDOW_WIDTH/2, 50 }, errorType);
+
+            GuiSetStyle(DEFAULT, TEXT_SIZE, 28);
+            if (GuiButton((Rectangle) { (WINDOW_WIDTH-WINDOW_WIDTH/4+50)/2, WINDOW_HEIGHT/2+200, WINDOW_WIDTH/4-50, 40 }, "Okay"))
+                showError = false;
         }
         
         EndDrawing();
